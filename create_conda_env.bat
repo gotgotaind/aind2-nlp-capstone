f07e82d0bb8564f220c8f052469aa0864a9d7bd0
gotgotaind <gotgotaind@gmail.com>
2017-08-08 20:18:32 +0200

    My initial commit. Just added environment creation and setup scripts.

---
+++ create_conda_env.bat
@@ -0,0 +1,5 @@
+conda create --name aind-nlp-capstone python=3.5 numpy
+activate aind-nlp-capstone
+pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/windows/gpu/tensorflow_gpu-1.2.1-cp35-cp35m-win_amd64.whl
+conda install scipy
+pip install keras -U
