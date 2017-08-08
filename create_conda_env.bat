153a10172eee045ce173fe890e8a39386fc3164f
gotgotaind <gotgotaind@gmail.com>
2017-08-08 23:39:12 +0200

    preprocessing seems ok

--- create_conda_env.bat
+++ create_conda_env.bat
@@ -3,3 +3,4 @@ activate aind-nlp-capstone
 pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/windows/gpu/tensorflow_gpu-1.2.1-cp35-cp35m-win_amd64.whl
 conda install scipy
 pip install keras -U
+conda install jupyter
